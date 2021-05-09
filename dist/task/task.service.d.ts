4489025ed4a780450ca769e78d0481ac7a5cec66
ritoshyadav <ritosh@resimpli.com>
2021-05-10 01:00:01 +0530

    remove node_module

--- dist/task/task.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Task } from './task.model';
-import { CreateTaskDto } from './dto/create-task.dto';
-export declare class TaskService {
-    private tasks;
-    getAllTasks(): Task[];
-    createTask(createTaskDto: CreateTaskDto): Task;
-    getTaskById(id: string): Task;
-}
