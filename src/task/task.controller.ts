dd4812c912b922b9907e747f21e78078d778944d
ritoshyadav <ritosh@resimpli.com>
2021-05-10 22:32:39 +0530

    add curd api

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Delete, Put, Patch, Query } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './task.model';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+import { Task, TaskStatus } from './task.model';
 import { TaskService } from './task.service';
 
 @Controller('task')
@@ -8,17 +9,34 @@ export class TaskController {
     constructor(private taskService: TaskService) { }
 
     @Get()
-    getAllTask(): Task[] {
-        return this.taskService.getAllTasks();
+    getTask(@Query() filterDto: GetTaskFilterDto): Task[] {
+        if (Object.keys(filterDto).length) {
+            return this.taskService.getTaskWithFilter(filterDto)
+        } else {
+            return this.taskService.getAllTasks();
+        }
     }
 
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.taskService.createTask(createTaskDto);
-
     }
+
     @Get('/:id')
     getTaskById(@Param('id') id: string): Task {
         return this.taskService.getTaskById(id);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string): void {
+        return this.taskService.deleteTask(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body('status') status: TaskStatus,
+    ): Task {
+        return this.taskService.updateTaskStatus(id, status);
     }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 
 @Injectable()
 export class TaskService {
@@ -9,6 +10,20 @@ export class TaskService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    getTaskWithFilter(filterDto: GetTaskFilterDto): Task[] {
+        const { status, search } = filterDto;
+        let tasks = this.getAllTasks();
+        if (status) {
+            tasks = tasks.filter(task => task.status === status)
+        }
+        if (search) {
+            tasks = tasks.filter(task =>
+                task.title.includes(search) || task.description.includes(search)
+            )
+        }
+        return tasks;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -26,5 +41,18 @@ export class TaskService {
 
     getTaskById(id: string): Task {
         return this.tasks.find(task => task.id == id);
+    }
+
+    deleteTask(id: string) {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        console.log("ststtts", status);
+
+        const taskData = this.getTaskById(id);
+        taskData.status = status;
+        console.log("tsaaa", taskData);
+
+        return taskData;
     }
 }
